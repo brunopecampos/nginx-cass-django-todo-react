0b479f6a1f9a8d83d64029331838d5c3fb95ccb0
brunopecampos <brunopecampos@protonmail.com>
2022-10-25 19:53:13 -0300

    Add animated gifs in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import opus from "./1555531463974.png";
+import money from "./breaking-bad-money.gif";
+import stonksImg from "./stonks.gif";
 import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
   const [percent, setPercent] = useState("");
+  const [stonks, setStonks] = useState(false);
+  const [bbMoney, setBbMoney] = useState(false);
 
   const increaseSalary = async () => {
     await fetch("http://localhost/increase-salary");
@@ -38,6 +42,23 @@ function App() {
     updatePercent();
   }, []);
 
+  useEffect(() => {
+    console.log("uepaaa");
+    if (percent === 100) {
+      setStonks(true);
+      console.log("stonks");
+    }
+    if (percent === 11273632) {
+      setBbMoney(true);
+      console.log("bbMoney");
+    }
+    if (percent === 0.5) {
+      setStonks(false);
+      setBbMoney(false);
+      console.log("Voltou");
+    }
+  }, [percent]);
+
   return (
     <div
       className="App"
@@ -47,7 +68,11 @@ function App() {
         paddingBottom: 350,
       }}
     >
-      <img src={opus} style={{ height: 300 }} />
+      <div>
+        {stonks ? <img src={stonksImg} style={{ height: 300 }} /> : <></>}
+        <img src={opus} style={{ height: 300 }} />
+        {bbMoney ? <img src={money} style={{ height: 300 }} /> : <></>}
+      </div>
       <p style={{ fontSize: 30 }}>
         <code>Percentual de aumento de salário após a apresentação:</code>
       </p>
